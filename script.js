b78c3038760bd13ed9952f1e2ff4841f40e90523
pelev1n-dev <pelev1n.dev@gmail.com>
2021-05-28 16:50:04 +0300

    lesson_2 simple is complete

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 90000;
+let income = 'Additional income';
+let addExpenses = 'Internet, Games, Trainings';
+let arrAddExpenses = addExpenses.toLowerCase().split(',');
+let deposit = true;
+let mission = 30000;
+let period = 12;
+let budgetDay = money / 30
 
-alert('a message with any text');
-console.log('a message with any text');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(`The period is ${period} months`);
+console.log(`The target to earn ${mission} euros`);
+console.log(arrAddExpenses);
+console.log(budgetDay);
