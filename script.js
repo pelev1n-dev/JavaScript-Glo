b4748be084e3383b6090cc9488ac0bca70b908f0
pelev1n-dev <pelev1n.dev@gmail.com>
2021-06-01 22:49:57 +0300

    Complete lessons 4

--- script.js
+++ script.js
@@ -1,47 +1,55 @@
 'use strict';
 
 let money = +prompt('Your monthly income?', '90000');
-console.log(typeof money);
-
 let income = 'Additional income';
-
 let addExpenses = prompt('List the possible expenses for the calculated period separated by commas', 'Internet, Games, Trainings');
-console.log(typeof addExpenses);
-console.log(addExpenses.length);
-console.log(addExpenses);
-
 let arrAddExpenses = addExpenses.toLowerCase().split(',');
-console.log(arrAddExpenses);
-
-let deposit = !!prompt('Do you have a bank deposit?', 'true');
-console.log(typeof deposit);
-
+let deposit = confirm('Do you have a bank deposit?');
+let mission = 3500000;
+let period = 12;
 let expenses1 = prompt('Enter the required expense item', 'Rent payment');
 let amount1 = +prompt('How much will it cost?','10000');
 let expenses2 = prompt('Enter the required expense item', 'Car');
 let amount2 = +prompt('How much will it cost?', '7500');
 
-let budgetMonth = money - (amount1 + amount2);
-console.log(budgetMonth);
+const showTypeOf = function (data) {
+  console.log(data, typeof(data));
+};
+showTypeOf(deposit);
 
-let mission = 3500000;
-console.log(`The target to earn ${mission} rubles`);
+let sumExpenses = 0;
+const getExpensesMonth = function (a, b) {
+  return sumExpenses = a + b;
+};
+console.log(getExpensesMonth(amount1, amount2));
+console.log(addExpenses);
 
-let missionMonth = Math.ceil(mission / budgetMonth);
-console.log(`The target will be achieved in ${missionMonth} months`);
+let accumulatedMonth = 0;
+const getAccumulatedMonth = function (a, b) {
+  return accumulatedMonth = a - b;
 
-let period = 12;
-console.log(`The period is ${period} months`);
+};
+getAccumulatedMonth(money, sumExpenses);
 
-let budgetDay = Math.floor(budgetMonth / 30);
+let budgetDay = Math.floor(accumulatedMonth / 30);
 console.log(budgetDay);
 
-if (budgetDay >= 1200) {
-  console.log('You have a high level of income');
-} else if (budgetDay >= 600 && budgetDay < 1200 ) {
-  console.log('You have an average income level');
-} else if (budgetDay >= 0 && budgetDay < 600) {
-  console.log('Unfortunately you have a lower than average income');
-} else {
-  console.log('Something went wrong');
-}
+const getTargetMonth = function (a, b) {
+  return Math.ceil(mission / accumulatedMonth);
+};
+console.log(getTargetMonth(mission, accumulatedMonth));
+
+let missionMonth = Math.ceil(mission / accumulatedMonth);
+
+const getStatusIncome = function () {
+  if (budgetDay >= 1200) {
+    console.log('You have a high level of income');
+  } else if (budgetDay >= 600 && budgetDay < 1200 ) {
+    console.log('You have an average income level');
+  } else if (budgetDay >= 0 && budgetDay < 600) {
+    console.log('Unfortunately you have a lower than average income');
+  } else {
+    console.log('Something went wrong');
+  }
+};
+getStatusIncome();
